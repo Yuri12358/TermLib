ffa61c1cab618e3d990fc3cc8f2498914e860fba
Yuri12358 <coder12358@gmail.com>
2019-05-07 22:03:18 +0300

    initial commit

---
+++ include/termlib/terminal_reader.hpp
@@ -0,0 +1,43 @@
+#ifndef TERMINAL_READER_HPP
+#define TERMINAL_READER_HPP
+
+#include"abstract_terminal_window.hpp"
+#include"default_window_provider.hpp"
+#include<string>
+
+class TerminalReader {
+public:
+    TerminalReader()
+        : win(DefaultWindowProvider::getWindow()) {}
+
+    explicit TerminalReader(AbstractTerminalWindow & window): win(window) {}
+
+    AbstractTerminalWindow & getTerminalWindow() const {
+        return win;
+    }
+
+    char readChar() {
+        return win.getChar().value();
+    }
+
+    std::optional<char> waitCharFor(int millis) {
+        return win.getChar(millis);
+    }
+
+    std::string getLine() {
+        std::string buffer;
+
+        char got = readChar();
+        while (got != '\n') {
+            buffer += got;
+            got = readChar();
+        }
+
+        return buffer;
+    }
+
+private:
+    AbstractTerminalWindow & win;
+};
+
+#endif // TERMINAL_READER_HPP
